4f974f734802b63f39d8d24dbf490e9c2aab2e4a
wesley331102 <59867382+wesley331102@users.noreply.github.com>
2020-07-29 10:37:21 +0800

    Add files via upload
    <feat> sample for readme.md

---
+++ spo-verification-program/src/test/java/com/itrustmachines/verification/sample/filePathSample.java
@@ -0,0 +1,27 @@
+package com.itrustmachines.verification.sample;
+
+import com.itrustmachines.common.config.EthereumNodeConfig;
+import com.itrustmachines.verification.VerificationApi;
+import com.itrustmachines.verification.vo.VerifyReceiptAndMerkleProofResult;
+import com.itrustmachines.verification.vo.VerifyVerificationProofResult;
+import java.util.List;
+
+public class filePathSample {
+
+    public static void main(String[] args) {
+
+        String contractAddress = "<contractAddress>";
+        String serverWalletAddress = "<serverWalletAddress>";
+        EthereumNodeConfig.Authentication auth = new EthereumNodeConfig.Authentication(false, "<username>", "<password>");
+        EthereumNodeConfig config = new EthereumNodeConfig("<privateKey>", "<nodeUrl>", "<infuraProjectIdEnv>", "<blockchainExplorerUrl>", auth);
+        VerificationApi api = VerificationApi.getInstance(contractAddress, serverWalletAddress, config);
+
+        String filePath = "<verificationProof.json檔之路徑>";
+        VerifyVerificationProofResult result = api.verify(filePath);
+        List<VerifyReceiptAndMerkleProofResult> proofResultList = result.getVerifyReceiptResults();
+
+        for (int i = 0; i < proofResultList.size(); i++) {
+            System.out.println(proofResultList.get(i).isPass());
+        }
+    }
+}
