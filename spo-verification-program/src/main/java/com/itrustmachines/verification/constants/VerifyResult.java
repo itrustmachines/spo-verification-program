2d0888ee862f46de894a40f0b7ac544e679d7019
fish360dogtv <fish360dogtv@gmail.com>
2021-12-14 15:24:18 +0800

    <feat> update version to 3.1.3.RC1

--- spo-verification-program/src/main/java/com/itrustmachines/verification/constants/VerifyResult.java
+++
@@ -1,7 +0,0 @@
-package com.itrustmachines.verification.constants;
-
-public enum VerifyResult {
-  
-  PASS, FAIL, PROOF_ERROR
-
-}
--- spo-verification-program/src/main/java/com/itrustmachines/verification/constants/VerifyVerificationProofStatus.java
+++
@@ -1,5 +0,0 @@
-package com.itrustmachines.verification.constants;
-
-public enum VerifyVerificationProofStatus {
-  ALL_PASS, SIG_ERROR, CONTRACT_CONNECTION_ERROR
-}
